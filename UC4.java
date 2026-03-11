718d16c92caaecf362721ddd71d27fc886a6cd29
Kiran4967 <kirantejaprakashofficial@gmail.com>
2026-03-11 15:59:57 +0530

    Coded UC4 to display Hello with Multiple Line Arguments

---
+++ UC4.java
@@ -0,0 +1,15 @@
+public class UC4{
+	public static void main(String[] args) {
+		StringBuilder nameBuilder = new StringBuilder();
+		for (int i = 0; i < args.length; i++) {
+			nameBuilder.append(args[i]);
+		    if (i < args.length - 1){
+				nameBuilder.append(",");
+			}
+			name = nameBuilder.toString();
+		}
+	}
+}
+			
+		
+		
